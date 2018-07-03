ed892997fe5aecbb4e6704a180e8ded09c930b03
admin <admin@gmail.com>
2018-07-03 15:59:32 +0300

    develop model structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div class="bg-success p-a-1 text-xs-center">
+                    This is SportsStore
+               </div>`
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent { }
